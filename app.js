0ee25e4851e4c93b2cf7a7626bf6e5de0e863b5b
TranTienDat <datdc21v7n602@vlvh.ctu.edu.vn>
2025-09-27 08:46:15 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const contactRoutes = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -11,5 +13,14 @@ app.get("/", (req, res) => {
 
 });
 app.use("/api/contacts", contactRoutes);
+app.use((req, res, next) => {
+return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+return res.status(err.statusCode || 500).json({
+message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
